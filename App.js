43d8619171a05cb61eab534f9202c77ec95abbe0
Abdull118 <naimi.airsh@gmail.com>
2024-05-28 17:41:54 -0400

    removed unnecessary code

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import React from 'react';
 import MyStack from './routes/MyStack';
 export default function App() {
 return (
-<NavigationContainer>
-<MyStack />
-</NavigationContainer>
+    <NavigationContainer>
+        <MyStack />
+    </NavigationContainer>
 );
 }
